66224c9eed48cfd3fcf151e8450dd57475bab691
Tommy <tommy.mccann001@gmail.com>
2025-05-02 18:44:33 +0100

    feat: add player name input field with edit/save toggle (no change handling yet)

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,15 +1,31 @@
+import { useState } from "react";
+
 type PlayerProps = {
   playerName: string;
   symbol: "X" | "O";
 };
 
 const Player = ({ playerName, symbol }: PlayerProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEdit = () => {
+    setIsEditing((prev) => !prev);
+    console.log("clicked");
+  };
+  const player = !isEditing ? (
+    <span className="player-name">{playerName}</span>
+  ) : (
+    <input value={playerName} autoFocus name={playerName} />
+  );
+
   return (
     <li>
       <span className="player">
-        {playerName}
+        {player}
         <span className="player-symbol">{symbol}</span>
-        <button>Edit</button>
+        <button onClick={() => handleEdit()}>
+          {isEditing ? "Save" : "Edit"}
+        </button>
       </span>
     </li>
   );
--- src/tests/__tests__/player.test.tsx
+++ src/tests/__tests__/player.test.tsx
@@ -1,21 +1,45 @@
 import { render, screen } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
 import Player from "../../components/Player";
 
-const mockPlayers = {
+const user = userEvent.setup();
+let mockPlayers = {
   X: "Player 1",
   O: "Player 2",
 };
 
+const componentUnderTest = <Player playerName={mockPlayers.X} symbol="X" />;
+
 describe("player component renders correctly", () => {
+  let rerenderComponent: (newProps?: React.ReactElement) => void;
+
+  // added so that state changes can be checked
+  beforeEach(() => {
+    const { rerender } = render(componentUnderTest);
+    // make rerender avaiable outside the beforeEach
+    rerenderComponent = rerender;
+  });
+
   test("player component shoulld render with default values", () => {
-    render(<Player playerName={mockPlayers.X} symbol="X" />);
     const player = mockPlayers.X;
     const playerName = screen.getByText(player);
     const playerSymbol = screen.getByText("X");
     expect(playerName).toHaveTextContent("Player 1");
     expect(playerSymbol).toBeInTheDocument();
   });
-  test("edit/save button toggle and correct input comes into focus", () => {});
+  test("edit/save button toggle and correct input comes into focus", async () => {
+    const editButton = screen.getByRole("button", { name: "Edit" });
+    const saveButton = screen.queryByRole("button", { name: "Save" });
+    expect(editButton).toBeInTheDocument();
+    expect(saveButton).not.toBeInTheDocument();
+    await user.click(editButton);
+    const newEditButton = screen.queryByRole("button", { name: "Edit" });
+    const newSaveButton = screen.getByRole("button", { name: "Save" });
+    const nameInput = screen.getByDisplayValue("Player 1");
+    expect(newEditButton).not.toBeInTheDocument();
+    expect(newSaveButton).toBeInTheDocument();
+    expect(nameInput).toBeInTheDocument();
+  });
   test("player input cannot be left blank", () => {});
   test("player name editable", () => {});
   test("player name saves when name is edited", () => {});
